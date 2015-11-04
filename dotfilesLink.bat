3a1fe39913ca1d9790f64aeccc5d95739b51d09e
tsukushibito <tsukushibito@gmail.com>
2015-11-05 04:02:54 +0900

    Fixed dotfilesLink.bat path

--- dotfilesLink.bat
+++ dotfilesLink.bat
@@ -1,5 +1,5 @@
-mklink %HOME%\.vimrc %HOME%\Documents\GitHub\dotfiles\.vimrc
-mklink %HOME%\.gvimrc %HOME%\Documents\GitHub\dotfiles\.gvimrc
-mklink /D %HOME%\.vim %HOME%\Documents\GitHub\dotfiles\.vim
+mklink %HOMEDRIVE%%HOMEPATH%\.vimrc %CD%\.vimrc
+mklink %HOMEDRIVE%%HOMEPATH%\.gvimrc %CD%\.gvimrc
+mklink /D %HOMEDRIVE%%HOMEPATH%\.vim %CD%\.vim
 
 git clone https://github.com/Shougo/neobundle.vim.git %HOME%.vim\bundle\neobundle.vim
