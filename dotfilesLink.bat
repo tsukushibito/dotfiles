c1b67c9b881b80bc945f596a1d6c944cae767b6a
tsukushibito <tsukushibito@gmail.com>
2016-12-18 04:27:20 +0900

    deinの自動クローンが動作しなかったので、バッチ側にクローンコマンドを追加

--- dotfilesLink.bat
+++ dotfilesLink.bat
@@ -1,3 +1,5 @@
 mklink %HOMEDRIVE%%HOMEPATH%\.vimrc %CD%\.vimrc
 mklink %HOMEDRIVE%%HOMEPATH%\.gvimrc %CD%\.gvimrc
 mklink /D %HOMEDRIVE%%HOMEPATH%\.config\vim %CD%\vim\
+
+git clone https://github.com/Shougo/dein.vim %HOMEDRIVE%%HOMEPATH%\.cache\vim\dein\repos\github.com\Shougo\dein.vim
