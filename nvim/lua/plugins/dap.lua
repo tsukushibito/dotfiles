5cc1c52218f4624f513709fbe025e6ea3b479be5
tsukushibito <tsukushibito@gmail.com>
2024-11-13 00:24:14 +0900

    dap関連のキーマップを追加

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -11,6 +11,8 @@ return {
       vim.keymap.set("n", "<leader>dn", function() dap.step_over() end, { desc = "Step Over" })
       vim.keymap.set("n", "<leader>di", function() dap.step_into() end, { desc = "Step Into" })
       vim.keymap.set("n", "<leader>do", function() dap.step_out() end, { desc = "Step Out" })
+      vim.keymap.set("n", "<leader>dmu", function() dap.up() end, { desc = "Move up in the stack trace" })
+      vim.keymap.set("n", "<leader>dmd", function() dap.down() end, { desc = "Move down in the stack trace" })
     end,
   },
   {
@@ -22,7 +24,14 @@ return {
       "nvim-neotest/nvim-nio",
     },
     config = function()
-      require("dapui").setup()
+      require("dapui").setup({
+        element_mappings = {
+          stacks = {
+            open = "<CR>",
+            expand = "o",
+          }
+        },
+      })
       local dap, dapui = require("dap"), require("dapui")
       dap.listeners.before.attach.dapui_config = function()
         dapui.open()
@@ -38,9 +47,35 @@ return {
       end
 
       vim.keymap.set("n", "<leader>du", function() dapui.toggle() end, { desc = "Toggle DAP UI" })
-      vim.keymap.set("n", "<leader>df", function()
-        dapui.float_element(nil, { enter = true, })
-      end, { desc = "Show DAP UI element in Floating Window" })
+      local args = { enter = true, }
+      vim.keymap.set('n', '<leader>de', function()
+        require('dapui').eval(nil, args)
+      end, { noremap = true, silent = true, desc = "Evaluate variable at cursor" })
+
+      -- Variables (scopes) window in a floating window
+      vim.keymap.set("n", "<leader>dfv", function()
+        dapui.float_element("scopes", args)
+      end, { noremap = true, silent = true, desc = "Show variables (scopes) in floating window" })
+
+      -- Watches window in a floating window
+      vim.keymap.set("n", "<leader>dfw", function()
+        dapui.float_element("watches", args)
+      end, { noremap = true, silent = true, desc = "Show watches in floating window" })
+
+      -- Breakpoints window in a floating window
+      vim.keymap.set("n", "<leader>dfb", function()
+        dapui.float_element("breakpoints", args)
+      end, { noremap = true, silent = true, desc = "Show breakpoints in floating window" })
+
+      -- Stack trace window in a floating window
+      vim.keymap.set("n", "<leader>dfs", function()
+        dapui.float_element("stacks", args)
+      end, { noremap = true, silent = true, desc = "Show stack trace in floating window" })
+
+      -- REPL (console) in a floating window
+      vim.keymap.set("n", "<leader>dfr", function()
+        dapui.float_element("repl", args)
+      end, { noremap = true, silent = true, desc = "Show REPL in floating window" })
     end,
   },
 }
