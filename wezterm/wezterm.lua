9c111af1de453df1807be325db8260b89a59ca6e
tsukushibito <tsukushibito@gmail.com>
2025-01-09 10:58:53 +0900

    wezterm.luaの更新とneogen.luaを追加

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -59,6 +59,37 @@ config.keys = {
   { key = "l", mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
   { key = "k", mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up' },
   { key = "j", mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down' },
+  { key = "p", mods = 'LEADER', action = wezterm.action.ShowLauncher },
 }
+
+launch_menu = {}
+if is_windows then
+    table.insert(launch_menu, {
+      label = 'PowerShell',
+      args = { 'powershell.exe', '-NoLogo' },
+    })
+
+    -- Find installed visual studio version(s) and add their compilation
+    -- environment command prompts to the menu
+    for _, vsvers in
+      ipairs(
+        wezterm.glob('Microsoft Visual Studio/20*', 'C:/Program Files (x86)')
+      )
+    do
+      local year = vsvers:gsub('Microsoft Visual Studio/', '')
+      table.insert(launch_menu, {
+        label = 'x64 Native Tools VS ' .. year,
+        args = {
+          'cmd.exe',
+          '/k',
+          'C:/Program Files (x86)/'
+            .. vsvers
+            .. '/BuildTools/VC/Auxiliary/Build/vcvars64.bat',
+        },
+      })
+    end
+end
+
+config.launch_menu = launch_menu
 
 return config
